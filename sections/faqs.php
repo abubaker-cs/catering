0a4bf0a7b8739e0bcd4e53f6322bf0e5273146ee
Mohammad Abubaker <Mohammad_Abubaker@yahoo.com>
2020-07-03 17:36:28 +0500

    FAQs Completed, DESC Remaining

--- sections/faqs.php
+++ sections/faqs.php
@@ -7,11 +7,114 @@
 <div id="sec-faqs" class="sec-container">
     <div class="grid-container">
 
-        <div class="grid-x grid-padding-x">
-            <div class="large-12 cell">
-                FAQs
-            </div>
-        </div><!-- /.grid-x grid-padding-x -->
+        <!-- TODO: Updated Description for FAQs -->
+        <section class="block-intro">
+            <h2 class="section-heading">FAQs</h2>
+            <p class="section-description lead"><?php echo COMPANY_NAME; ?> is a corporate catering company
+                which specializes in onshore and offshore hospitality services for both local and multinational Oil &
+                Gas Companies operating in Libya.</p>
+        </section>
+
+
+        <ul id="faqs-list" class="accordion" data-accordion="accordion" data-allow-all-closed="true" role="tablist">
+
+            <li class="accordion-item" data-accordion-item="">
+                <a href="#" class="accordion-title" aria-controls="accordion" role="tab" id="accordion-1"
+                   aria-expanded="false" aria-selected="false">Q1 - Why is Corporate Catering Important for your Oil
+                    & Gas Company?</a>
+
+                <div class="accordion-content" data-tab-content="" role="tabpanel" aria-labelledby="accordion-1"
+                     aria-hidden="true" id="k66eaj-accordion">
+                    <p>The basic purpose of corporate catering is to provide food with convenience. At Lamasat
+                        elKheir for Catering Services our professional chefs take care of all your food and beverage
+                        requirements for any scale of corporate catering project. So that you can fully focus on
+                        your business operations without worrying about the catering operations. </p>
+                </div>
+
+            </li>
+
+            <li class="accordion-item" data-accordion-item="">
+                <a href="#" class="accordion-title" aria-controls="accordion" role="tab" id="accordion-2"
+                   aria-expanded="false" aria-selected="false">Q2 - Why chose you?</a>
+
+                <div class="accordion-content" data-tab-content="" role="tabpanel" aria-labelledby="accordion-2"
+                     aria-hidden="true" id="k66eaj-accordion">
+                    <ul>
+                        <li>At Lamasat elKheir for Catering Services quality and customer’s satisfaction continue to
+                            be our number one priority in any operation we undertake.
+                        </li>
+                        <li>Lamasat elKheir for Catering Services selects quality ingredients and well-balanced
+                            combinations of flavours, together with its attention to details.
+                        </li>
+                    </ul>
+                </div>
+
+            </li>
+
+            <li class="accordion-item" data-accordion-item="">
+                <a href="#" class="accordion-title" aria-controls="accordion" role="tab" id="accordion-3"
+                   aria-expanded="false" aria-selected="false">Q3 - What Kind of Catering Services Do You Offer?</a>
+
+                <div class="accordion-content" data-tab-content="" role="tabpanel" aria-labelledby="accordion-3"
+                     aria-hidden="true" id="k66eaj-accordion">
+                    <p>Lamasat elKheir for Catering Services has a highly experienced team that is extensively
+                        trained to provide services as per the client’s requirements. Having several years of
+                        experience, Lamasat elKheir for Catering Services is well equipped to serve any scale of the
+                        project. We provide different types of catering services for onshore and offshore companies
+                        operating in Libyan Oil & Gas industries, such as:</p>
+
+                    <ul>
+                        <li>Breakfast, lunch and dinner</li>
+                        <li>Corporate BBQs / Buffets</li>
+                        <li>Offshore Catering Services</li>
+                        <li>Onshore Catering Services</li>
+                        <li>Packed Meal Boxes</li>
+                        <li>Snacks and menus for seminars and business conferences</li>
+                        <li>Tea / Coffee breaks</li>
+                    </ul>
+
+                    <p>Each type of catering requires a different selection of menus. At Lamasat elKheir for
+                        Catering Services our focus is on providing the highest quality of food and services for our
+                        esteemed guests.</p>
+                </div>
+
+            </li>
+
+            <li class="accordion-item" data-accordion-item="">
+                <a href="#" class="accordion-title" aria-controls="accordion" role="tab" id="accordion-4"
+                   aria-expanded="false" aria-selected="false">Q4 - What is your corporate culture?</a>
+
+                <div class="accordion-content" data-tab-content="" role="tabpanel" aria-labelledby="accordion-4"
+                     aria-hidden="true" id="k66eaj-accordion">
+                    <p>New initiatives are specifically designed to enhance overall employee well-being and morale
+                        because as a company we are committed towards persistently investing in our people,
+                        developing their skill sets and talents with the necessary support and assistance. </p>
+
+                    <p>The central ethos of our “Corporate Culture” is the creativity and passion of our employees.
+                        Our teams are encouraged to contribute their thoughts and idea solutions to help the Lamasat
+                        elKheir for Catering Services grow and reach greater heights. </p>
+                </div>
+
+            </li>
+
+            <li class="accordion-item" data-accordion-item="">
+                <a href="#" class="accordion-title" aria-controls="accordion" role="tab" id="accordion-4"
+                   aria-expanded="false" aria-selected="false">Q5 - What are the benefits of partnering with Lamasat
+                    elKheir for Catering Services?</a>
+
+                <div class="accordion-content" data-tab-content="" role="tabpanel" aria-labelledby="accordion-4"
+                     aria-hidden="true" id="k66eaj-accordion">
+                    <ul>
+                        <li>Customized menus by executive chef</li>
+                        <li>Versatility of service</li>
+                        <li>Emphasis on safety through mandatory specialized training of staff</li>
+                        <li>Innovative catering solutions</li>
+                        <li>Rapid response and mobilization of resources for large projects</li>
+                    </ul>
+                </div>
+
+            </li>
+        </ul>
 
     </div><!-- /.grid-container -->
 </div><!-- /#sec-faqs .sec-container -->
