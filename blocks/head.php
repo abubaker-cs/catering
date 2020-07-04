96badd993282f09125cf6251b6b044c3f3e1738f
Mohammad Abubaker <Mohammad_Abubaker@yahoo.com>
2020-07-04 16:47:06 +0500

    RWD Menu Implemented.

--- blocks/head.php
+++ blocks/head.php
@@ -4,15 +4,27 @@
         LOGO
     </div>
 
-    <ul class="menu expanded align-center">
-        <li><a href="#sec-about">About</a></li>
-        <li><a href="#sec-ceo">Message</a></li>
-        <li><a href="#sec-services">Services</a></li>
-        <li><a href="#sec-why">Why us?</a></li>
-        <li><a href="#sec-hseq">HSEQ Policies</a></li>
-        <li><a href="#sec-different">Being Different</a></li>
-        <li><a href="#sec-faqs">FAQs</a></li>
-        <li><a href="#sec-contact">Contact</a></li>
-    </ul>
+    <div class="title-bar" data-responsive-toggle="example-menu" data-hide-for="large">
+        <button class="menu-icon" type="button" data-toggle="example-menu"></button>
+        <div class="title-bar-title">Menu</div>
+    </div>
+
+    <div class="top-bar" id="example-menu">
+        <div class="top-bar-left">
+            <ul class="menu expanded align-center vertical large-horizontal text-center" data-dropdown-menu>
+
+                <li><a href="#sec-about">About</a></li>
+                <li><a href="#sec-ceo">Message</a></li>
+                <li><a href="#sec-services">Services</a></li>
+                <li><a href="#sec-why">Why us?</a></li>
+                <li><a href="#sec-hseq">HSEQ</a></li>
+                <li><a href="#sec-different">Being Different</a></li>
+                <li><a href="#sec-faqs">FAQs</a></li>
+                <li><a href="#sec-contact">Contact</a></li>
+
+            </ul>
+        </div>
+
+    </div>
 
 </div><!-- /#sec-header-block .sec-container -->
